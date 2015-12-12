9db7c8aea9b2492c84d2fb8bed4dd83d90489b04
skanca <senol.kanca34@gmail.com>
2015-12-12 14:50:26 +0200

    Mesajlar almaya başladım.

--- Client/Client.py
+++ Client/Client.py
@@ -1,15 +1,14 @@
 import socket
-import sys
 import threading
+import Queue
+import time
+import sys
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-import Queue
-import time
-
 
 class ReadThread (threading.Thread):
-    def __init__(self,name,csoc,threadQueue,screenQueue,app):
+    def __init__(self, name,csoc,threadQueue,screenQueue,app):
         threading.Thread.__init__(self)
         self.name = name
         self.csoc = csoc
@@ -18,35 +17,55 @@ class ReadThread (threading.Thread):
         self.screenQueue = screenQueue #...
         self.app = app
 
+    #incoming parser
     def incoming_parser(self,data):
+        print ('INCOMING DATA: ', data)
+
         if len(data) == 0:
             return "Error!!!"
-        if len(data) > 3 and not data[3] == "":
+        if len(data) < 3 and not data[3] == "":
             response = "ERR"
             self.csoc.send(response)
             return "Error"
         if data[0:3] == "HEL":
-            #Yeni Kullanıcı olarak kabul edildiniz
+            inMessage=  'Yeni Kullanici olarak kabul edildiniz'
         elif data[0:3] == "REJ":
-            #Kullanıcı olarak kabul edilMEdiniz
-        elif data[0:3] == "BYE":
-            #Çıkış
-        elif data[0:3] == "LSA":
-            #user List = data[4:] Kullanıcı Listesi geldi
-        elif data[0:3] == "TOC":
-            #Bağlantı testi başarılı
-        elif data[0:3] == "SOK":
-            #Genel mesaj gönderme başarılı
-        elif data[0:3] == "MOK":
-            #Özel mesaj gönderme başarılı
-        elif data[0:3] == "MNO":
-            #Özel mesaj hedefi bulunamadı
-        elif data[0:3] == "ERR":
-            #Hatalı Komut gönderdiniz
-        elif data[0:3] == "ERL":
-            #Giriş yapılaMAdı; login olmalısınız
-        #...
+            inMessage = 'Kullanici olarak kabul edilMEdiniz'
 
+        elif data[0:3] == "BYE":
+            inMessage = 'Cikis'
+
+        elif data[0:3] == "LSA":
+            inMessage = 'LSA'
+
+        elif data[0:3] == "TOC":
+            inMessage = 'Baglanti testi basarili'
+
+        elif data[0:3] == "SOK":
+            inMessage = 'Genel mesaj gonderme basarili'
+
+        elif data[0:3] == "MOK":
+            inMessage = 'Ozel mesaj gonderme basarili'
+
+        elif data[0:3] == "MNO":
+            inMessage = 'Ozel mesaj hedefi bulunamadi'
+
+        elif data[0:3] == "ERR":
+            inMessage = 'Hatali Komut gonderdiniz'
+
+        elif data[0:3] == "ERL":
+            inMessage = 'Giris yapilaMAdi; login olmalisiniz'
+
+        else:
+            response = "ERR"
+            self.csoc.send(response)
+            return "Error"
+        inMessage = time.strftime("[%H:%M:%S]", time.gmtime()) + inMessage
+        print("inMessage = ",inMessage)
+        return inMessage
+
+
+#
     def run(self):
         while True:
             try:
@@ -56,7 +75,6 @@ class ReadThread (threading.Thread):
                 pass
             if data[0:3] == "BYE":
                 self.csoc.close()
-            #...
 
 class WriteThread (threading.Thread):
     def __init__(self,name,csoc, threadQueue):
@@ -64,18 +82,25 @@ class WriteThread (threading.Thread):
         self.name = name
         self.csoc = csoc
         self.threadQueue = threadQueue
-
     def run(self):
         while True:
             if self.threadQueue.qsize() > 0:
                 queue_message = self.threadQueue.get()
                 try:
+                    print("queue_message= ",str(queue_message))
                     self.csoc.send(queue_message)
                 except socket.error:
                     self.csoc.close()
+                    break
 
+#
 class ClientDialog(QDialog):
+
     def __init__(self,threadQueue,screenQueue):
+        """
+
+        :rtype: object
+        """
         self.threadQueue = threadQueue
         self.screenQueue = screenQueue
 
@@ -98,11 +123,12 @@ class ClientDialog(QDialog):
         self.hbox = QHBoxLayout()
 
         #the sender textbox
-        self.sender = QLineEdit("",self)
+        self.sendMessage = QLineEdit("",self)
+
 
         #the channel region
-        self.channel = QTextBrowser()
-        self.channel.setMinimumSize(QSize(480,0))
+        self.messageArea = QTextBrowser()
+        self.messageArea.setMinimumSize(QSize(480,0))
 
         #the send button
         self.send_button = QPushButton('&Send')
@@ -111,13 +137,14 @@ class ClientDialog(QDialog):
         self.userList = QListView()
 
         #connect the Go button to its callback
-        self.send_button.click(self.outgoing_parser)
+        self.send_button.connect(self.send_button,SIGNAL('clicked()'),self.outgoing_parser)
 
         #add the controls to the vertical layout
         self.vbox.addLayout(self.hbox)
-        self.vbox.addWidget(self.sender)
+        self.vbox.addWidget(self.sendMessage)
         self.vbox.addWidget(self.send_button)
-        self.hbox.addWidget(self.channel)
+        self.hbox.addWidget(self.messageArea)
+
         self.hbox.addWidget(self.userList)
 
         #start timer
@@ -130,46 +157,54 @@ class ClientDialog(QDialog):
         self.setLayout(self.vbox)
 
     def cprint(self,data):
-        self.channel.append(data)
+        #self.screenQueue.put(data)
+        self.messageArea.append(data)
 
     def updateChannelWindow(self):
         if self.screenQueue.qsize() > 0:
             queue_message = self.screenQueue.get()
-            #...
-            #...
-            #...
-            #self.channel.append(...)
+            print('queue_message: ',queue_message)
+            # #...
+            # #...
+            # #...
+            # #self.channel.append(...)
+
+
     def outgoing_parser(self):
-        #...
-        #...
-        #...
+        #self.messageArea.append("Hello QTextBrowser")
+        #self.messageArea.append(self.sendMessage.text())
+        outMessage = self.sendMessage.text()
+        print(outMessage)
+        self.threadQueue.put(str(outMessage))
+        self.sendMessage.clear()
+
+
     def run(self):
         #run the app and show the main form
         self.show()
         self.qt_app.exec_()
 
-    #connect to the server
-    s = socket.socket()
-    #host = ...
-    #port = ...
-    s.connect((host,port))
 
-    #sendQueue = ...
-    #screenQueue = ...
+#connect to the server
+s = socket.socket()
+host = '178.233.19.205'
+port = 12345
+s.connect((host,port))
+sendQueue = Queue.Queue()
+screenQueue = Queue.Queue()
+app = ClientDialog (sendQueue,screenQueue)
+#start threads
+rt = ReadThread("ReadThread",s,sendQueue,screenQueue,app)
+rt.start()
+    #
+wt = WriteThread("WriteThread",s,sendQueue)
+wt.start()
+    #
+app.run()
 
-    #app = ClientDialog(sendQueue,screenQueue)
-
-    #start threads
-    rt = ReadThread("ReadThread",s,sendQueue,screenQueue)
-    rt.start()
-
-    wt = WriteThread("WriteThread",s,sendQueue)
-    wt.start()
-
-    app.run()
-    rt.join()
-    wt.join()
-    s.close()
+rt.join()
+wt.join()
+s.close()
 
 
 
